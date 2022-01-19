b1f0aa7362d277359b8e9e97ac939a4a73faa007
Tam <tam@Tam.Computer>
2022-01-19 15:56:11 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+$('.navItem').each(function() {
+	$(this).css('color', 'white');
+});
+$('#nav').css('background-color', '#145');
+$('li:even').css('background-color', '#aaa');
+$('li:odd').css('background-color', '#ccc');
+$('input').val('Terry');
